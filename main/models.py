9fcbd9f9769f53ecc0eddb9374901d4ab0e7299d
plutasnyy <kamil.plucinski97@gmail.com>
2019-01-07 19:33:27 +0100

    added png to allowed types

--- main/models.py
+++ main/models.py
@@ -1,11 +1,15 @@
+from django.core.validators import FileExtensionValidator
 from django.db import models
 from ckeditor.fields import RichTextField
 
+
 class ImageModel(models.Model):
-    mainimage = models.ImageField(upload_to='img', null=True)
-    title = models.CharField(max_length=100,primary_key=True)
+    mainimage = models.ImageField(upload_to='img', null=True, validators=[FileExtensionValidator(['jpg', 'png'])])
+    title = models.CharField(max_length=100, primary_key=True)
+
     def __str__(self):
         return self.title
+
 
 class Post(models.Model):
     id = models.AutoField(primary_key=True)
@@ -18,5 +22,3 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
-
-
