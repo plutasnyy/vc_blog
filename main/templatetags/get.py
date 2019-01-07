3f605c1bcde787c4136a2400ade70b8f9c4b8e5c
plutasnyy <kamil.plucinski97@gmail.com>
2019-01-07 21:07:41 +0100

    small fixes

--- main/templatetags/get.py
+++ main/templatetags/get.py
@@ -1,9 +1,8 @@
 from django import template
 
 register = template.Library()
-from main.models import ImageModel
 
 
 @register.filter(name='get')
-def get(Images,name):
-    return Images.filter(title=str(name))[0].mainimage.url
+def get(images, name):
+    return images.filter(title=str(name))[0].mainimage.url
